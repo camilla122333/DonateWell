79773784655b5e59d972ef04b498b536171bd94b
camilla <pasadenastringtheory@gmail.com>
2022-07-28 18:29:39 -0700

    Initial Commit

---
+++ DonateWell/DonateWellApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonateWellApp.swift
+//  DonateWell
+//
+//  Created by Camilla B on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct DonateWellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
