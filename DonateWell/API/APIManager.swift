b4f7c942bdeb7874a7cd30bc042ef636ab372997
camilla <pasadenastringtheory@gmail.com>
2022-08-09 10:29:35 -0700

    Fixed external API call

--- DonateWell/API/APIManager.swift
+++ DonateWell/API/APIManager.swift
@@ -8,19 +8,11 @@
 import Foundation
 import SwiftUI
 
-//error handling
-//
-// create Decodable class
-// // https://stackoverflow.com/questions/53569247/how-to-use-decodable-in-swift
-//class APICall {
-// method to get URL, parse data with async
-//
-
 class APIManager: ObservableObject {
-//    
-//    init() {
-//        getMarket()
-//    }
+    
+    init() {
+        getMarket()
+    }
     
     func getMarket() {
         let url = URL(string: "https://www.predictit.org/api/marketdata/markets/7017")!
@@ -39,7 +31,7 @@ class APIManager: ObservableObject {
             let decoder = JSONDecoder()
             // try!
             let values = try! decoder.decode(Response.self, from: data)
-            print(values)
+            print(values.shortName)
 
 
         }
