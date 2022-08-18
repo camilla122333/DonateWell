6bbf9e4b68622e84f0c6585e6df858c640fdce3c
camilla <pasadenastringtheory@gmail.com>
2022-08-17 23:25:42 -0700

    improves web experience, changes twitter feed

--- DonateWell/Views/Page3.swift
+++ DonateWell/Views/Page3.swift
@@ -11,15 +11,9 @@ import WebKit
 //<a class="twitter-timeline" href="https://twitter.com/Politics_Polls?ref_src=twsrc%5Etfw">Tweets by Politics_Polls</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
 struct Page3: View {
-    @State var text = "<a class=\"twitter-timeline\" href=\"https://twitter.com/Politics_Polls?ref_src=twsrc%5Etfw\">Tweets by Politics_Polls</a> <script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>"
     
-//    init() {
-//        UINavigationBar.appearance().backgroundColor = .systemPink
-//
-//        UINavigationBar.appearance().largeTitleTextAttributes = [
-//            .foregroundColor: UIColor.white,
-//            .font : UIFont(name:"Helvetica Neue", size: 40)!]
-//    }
+    @State var text = "<a class=\"twitter-timeline\" href=\"https://twitter.com/asterouter/lists/1560148054447034368?ref_src=twsrc%5Etfw\">A Twitter List</a> <script async src=\"https://platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>"
+    
     
     var body: some View {
         
@@ -29,7 +23,6 @@ struct Page3: View {
                 .background(Color(hue: 0.494, saturation: 0.138, brightness: 0.964, opacity: 0.941))
                 .edgesIgnoringSafeArea(.all)
         }
-//        .navigationBarTitle("Latest Polls")
     }
 }
 
--- DonateWell/Views/RecommendationView.swift
+++ DonateWell/Views/RecommendationView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import WebKit
 
 // rename
 //class ArrayModificationViewModel: ObservableObject {
@@ -35,6 +36,7 @@ struct RecommendationView: View {
 //    @StateObject var vm = ArrayModificationViewModel()
     @State var num = 1
     @State var filteredArray: [Senate_Race] = []
+    @State private var showActBlueView = false
 
     var body: some View {
         VStack (spacing: 20){
@@ -45,7 +47,7 @@ struct RecommendationView: View {
             ScrollView {
                 VStack(spacing: 20) {
                     ForEach(dataManager.races.filter { $0.isRecommended }) { election in
-                        HStack {
+                        HStack (spacing: 5) {
                             VStack(alignment: .leading) {
                                 Text(election.state)
                                     .font(.headline)
@@ -57,8 +59,6 @@ struct RecommendationView: View {
                                     Spacer()
                                     if election.isRecommended {
                                         Image(systemName: "star.fill")
-    //                                        .resizable()
-    //                                        .frame(width: 40, height: 40)
                                             .padding(.horizontal)
                                             .foregroundColor(.cyan)
                                     }
@@ -68,21 +68,26 @@ struct RecommendationView: View {
                             .padding()
                             .background(Color.orange.cornerRadius(10))
                             .padding(.horizontal)
-                            VStack {
-                                ZStack {
-                                    
-                                    Link(destination: URL(string: election.link)!) {
-                                        Circle()
-    //                                        .stroke(.black, lineWidth: 1)
-                                            .frame(width: 50, height: 50)
-                                        .foregroundColor(.white)
+                            VStack(spacing: 12) {
+                                
+                                Button(action: {
+                                    showActBlueView.toggle()
+                                    }, label: {
+                                        Image(systemName: "heart")
+                                            .resizable()
+                                            .frame(width: 28, height: 28)
+                                            .foregroundColor(.black)
+                                            .background(
+                                                Circle()
+                                                    .frame(width: 45, height: 45)
+                                                .foregroundColor(.white)
+                                            )
                                     }
-                                    Image(systemName: "heart")
-                                        .resizable()
-                                        .frame(width: 28, height: 28)
-                                        .foregroundColor(.black)
+                                )
+                                .sheet(isPresented: $showActBlueView) {
+                                    WebViewActBlue(url: URL(string: election.link)!)
                                 }
-                                    
+                                                                
                                 Text("Donate")
                                     .font(.system(size: 13))
                                     .foregroundColor(.red)
@@ -97,8 +102,7 @@ struct RecommendationView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(hue: 0.494, saturation: 0.138, brightness: 0.964, opacity: 0.941))
     }
-    
-    
+        
 //    func updateFilteredArray() {
 //        let electionsArr = dataManager.races
 //        filteredArray = electionsArr.sorted(by: {
@@ -107,6 +111,19 @@ struct RecommendationView: View {
 //        print("FILTERED ARRAY****: \(filteredArray)")
 //    }
 }
+
+struct WebViewActBlue: UIViewRepresentable {
+    var url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+ }
 
 struct RecommendationView_Previews: PreviewProvider {
     static var previews: some View {
